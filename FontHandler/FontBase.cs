ec1038dc091e0da7c46734c5a63a43115d5f9d0b
burst99325 <burst99325@gmail.com>
2022-07-15 18:05:59 +0200

    1.0

--- FontHandler/FontBase.cs
+++ FontHandler/FontBase.cs
@@ -7,7 +7,7 @@ namespace FontHandler
         public Font LoadFont(FontType fontType, string filePath)
         {
             Type type = fontType.Assembly.GetType("FontHandlerFormat.FontHandlerFont");
-            if (type == null) throw new Exception(String.Format("This FontHandler plugin cannot load fonts"));
+            if (type == null) throw new Exception("This FontHandler plugin cannot load fonts!");
             var obj = Activator.CreateInstance(type);
             return (Font)type.GetMethod("Load").Invoke(obj, new object[] { filePath });
         }
@@ -15,7 +15,7 @@ namespace FontHandler
         public void SaveFont(Font font, string filePath)
         {
             Type type = font.Type.Assembly.GetType("FontHandlerFormat.FontHandlerFont");
-            if (type == null) throw new Exception(String.Format("This FontHandler plugin cannot save fonts"));
+            if (type == null) throw new Exception("This FontHandler plugin cannot save fonts!");
             var obj = Activator.CreateInstance(type);
             type.GetMethod("Save").Invoke(obj, new object[] { font, filePath });
         }
